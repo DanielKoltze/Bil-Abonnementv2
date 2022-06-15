0668a0b4bf2b32248ddfeacd96975fabe2ffe448
DanielKoltze <dani64h6@stud.kea.dk>
2022-06-15 15:42:15 +0200

    test

---
+++ src/test/java/com/example/bilabonnement/controllers/DamageRapportDemoTest.java
@@ -0,0 +1,78 @@
+package com.example.bilabonnement.controllers;
+
+import com.example.bilabonnement.models.DamageReport;
+import com.example.bilabonnement.repositories.DamageReportRepository;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DamageRapportDemoTest {
+
+
+    private static DamageReportRepository damageReportRepository;
+    private static WebDriver driver;
+    private static DamageReport testDamageReport;
+    private static String testChassisNumber;
+    private static String testRegistrationNumber;
+    private static String baseUrl;
+
+
+
+    @BeforeAll()
+    public static void setupAndPing() {
+        WebDriverManager.chromedriver().setup();
+
+        damageReportRepository = new DamageReportRepository();
+
+        testChassisNumber = "demo";
+        testRegistrationNumber = "demo";
+
+
+        baseUrl = "https://bil-abonnement-a.herokuapp.com";
+
+    }
+
+    public static void login() {
+        driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+
+        username.sendKeys("skade");
+        password.sendKeys("skade");
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+    }
+
+    @Test
+    @Order(1)
+    public void createDamageReport() {
+        login();
+
+        driver.get(baseUrl + "show-damagereport/" + testChassisNumber + "/" + testRegistrationNumber);
+
+        WebElement createDamageReportButton = driver.findElement(By.id("create-damage-report-button"));
+        WebElement damageReportDescription = driver.findElement(By.id("description"));
+
+        for (int i = 0; i < 13; i++) {
+            damageReportDescription.sendKeys("Parkeringsskade " + i+1);
+            createDamageReportButton.click();
+        }
+
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        driver.close();
+        driver.quit();
+    }
+
+}
